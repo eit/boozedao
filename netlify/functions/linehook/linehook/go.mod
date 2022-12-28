aec6f701447ac34e2dfaf56fc4cd314244420a20
eit <cheneit@gmail.com>
2022-12-28 16:56:02 +0800

    init linebot

---
+++ netlify/functions/linehook/linehook/go.mod
@@ -0,0 +1,9 @@
+module github.com/eit/boozedao/linehook
+
+go 1.19
+
+require (
+	github.com/aws/aws-lambda-go v1.35.0
+	github.com/joho/godotenv v1.4.0
+	github.com/line/line-bot-sdk-go/v7 v7.18.0
+)
---
+++ netlify/functions/linehook/linehook/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+
+	// _ "github.com/joho/godotenv/autoload"
+	"github.com/line/line-bot-sdk-go/v7/linebot"
+)
+
+func handler(ctx context.Context, req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	fmt.Println("This message will show up in the CLI console.")
+	// fmt.Println(os.Getenv("CHANNEL_SECRET"), os.Getenv("CHANNEL_ACCESS_TOKEN"))
+	bot, err := linebot.New(os.Getenv("CHANNEL_SECRET"), os.Getenv("CHANNEL_ACCESS_TOKEN"))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	r := proxyRequest2httpRequest(&req)
+	event_set, err := bot.ParseRequest(r)
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			return &events.APIGatewayProxyResponse{
+				StatusCode:      400,
+				Headers:         map[string]string{"Content-Type": "text/plain"},
+				IsBase64Encoded: false,
+			}, nil
+		} else {
+			return &events.APIGatewayProxyResponse{
+				StatusCode:      500,
+				Headers:         map[string]string{"Content-Type": "text/plain"},
+				IsBase64Encoded: false,
+			}, nil
+		}
+	}
+
+	for _, event := range event_set {
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			// Handle only on text message
+			case *linebot.TextMessage:
+
+			// Handle only on Sticker message
+			case *linebot.StickerMessage:
+				var kw string
+				for _, k := range message.Keywords {
+					kw = kw + "," + k
+				}
+
+				outStickerResult := fmt.Sprintf("收到貼圖訊息: %s, pkg: %s kw: %s  text: %s", message.StickerID, message.PackageID, kw, message.Text)
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(outStickerResult)).Do(); err != nil {
+					log.Print(err)
+				}
+			}
+		}
+	}
+
+	return &events.APIGatewayProxyResponse{
+		StatusCode:      200,
+		Headers:         map[string]string{"Content-Type": "text/plain"},
+		Body:            "Hello, world!",
+		IsBase64Encoded: false,
+	}, nil
+}
+
+func proxyRequest2httpRequest(request *events.APIGatewayProxyRequest) *http.Request {
+	httpRequest, err := http.NewRequest(
+		strings.ToUpper(request.HTTPMethod),
+		request.Path,
+		bytes.NewReader([]byte(request.Body)),
+	)
+
+	if err != nil {
+		fmt.Printf("Convert To Request Failed")
+	}
+
+	if request.MultiValueHeaders != nil {
+		for k, values := range request.MultiValueHeaders {
+			for _, value := range values {
+				httpRequest.Header.Add(k, value)
+			}
+		}
+	} else {
+		for h := range request.Headers {
+			httpRequest.Header.Add(h, request.Headers[h])
+		}
+	}
+
+	httpRequest.RequestURI = httpRequest.URL.RequestURI()
+
+	return httpRequest
+}
+
+func main() {
+	lambda.Start(handler)
+}
